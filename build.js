4e52a59d7a7be1eff7e8c99d2f87d799b38f2a77
Denis Antonenko <denis.antonenko@dataart.com>
2018-02-01 15:50:54 +0200

    Build file

---
+++ build.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const browserify = require('browserify');
+const exorcist = require('exorcist');
+
+const srcDir = __dirname;
+const outputDir = path.join(__dirname, 'dist');
+
+const outputProd = path.join(outputDir, 'devicehive.js');
+const outputDev = path.join(outputDir, 'devicehive.min.js');
+const mapDev = path.join(outputDir, 'devicehive.min.js.map');
+
+const bundlerProd = browserify(path.join(srcDir, 'index-browser.js'));
+const bundlerDev = browserify(path.join(srcDir, 'index-browser.js'), {
+    debug: true
+});
+
+// Creating dist directory
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
+// preset
+
+bundlerProd
+    .transform('babelify')
+
+bundlerDev
+    .transform('babelify')
+    .transform('uglifyify', {
+        global: true,
+        sourceMap: true
+    });
+
+
+// build
+
+bundlerProd.bundle()
+    .pipe(fs.createWriteStream(outputProd), 'utf8')
+    .on('finish', () => {
+        console.log('\x1b[36m%s\x1b[0m', 'Production build complete!');
+    });
+
+bundlerDev.bundle()
+    .pipe(exorcist(mapDev))
+    .pipe(fs.createWriteStream(outputDev), 'utf8')
+    .on('finish', () => {
+        console.log('\x1b[36m%s\x1b[0m', 'Development build complete!');
+    });
