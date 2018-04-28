8725c1a74c20d4f77234118676228493e6cf6894
Grigoriy <antgrisha@gmail.com>
2018-04-28 15:09:45 +0300

    updated integration tests

--- test/integration/controllers/DeviceTypeAPI.spec.js
+++ test/integration/controllers/DeviceTypeAPI.spec.js
@@ -4,6 +4,7 @@ const assert = chai.assert;
 const config = require('../config');
 const DeviceHive = require('../../../index');
 const DeviceType = DeviceHive.models.DeviceType;
+const Device = DeviceHive.models.Device;
 const DeviceTypeListQuery = DeviceHive.models.query.DeviceTypeListQuery;
 const DeviceTypeCountQuery = DeviceHive.models.query.DeviceTypeCountQuery;
 const DeviceTypeDeleteQuery = DeviceHive.models.query.DeviceTypeDeleteQuery;
@@ -22,6 +23,28 @@ const TEST_DEVICE_TYPES = {
     WS: {
         name: `${TEST_DEVICE_TYPE_NAME_PREFIX}${randomString.generate()}`,
         description: `${TEST_DEVICE_TYPE_DESCRIPTION_PREFIX}${randomString.generate()}`
+    }
+};
+
+const TEST_DEVICE_ID_PREFIX = `DH-JS-LIB-DEVICE-ID-`;
+const TEST_DEVICE_NAME_PREFIX = `DH-JS-LIB-DEVICE-NAME-`;
+
+const TEST_DEVICES = {
+    HTTP: {
+        id: `${TEST_DEVICE_ID_PREFIX}HTTP`,
+        name: `${TEST_DEVICE_NAME_PREFIX}HTTP`,
+        networkId: 1,
+        deviceTypeId: 1,
+        isBlocked: false,
+        data: {}
+    },
+    WS: {
+        id: `${TEST_DEVICE_ID_PREFIX}WS`,
+        name: `${TEST_DEVICE_NAME_PREFIX}WS`,
+        networkId: 1,
+        deviceTypeId: 1,
+        isBlocked: false,
+        data: {}
     }
 };
 
@@ -159,6 +182,59 @@ describe('DeviceTypeAPI', () => {
         wsDeviceHive.deviceType.delete(deviceTypeDeleteQuery)
             .then(() => done())
             .catch(done);
+    });
+
+    it(`should delete device type with name: ${TEST_DEVICE_TYPES.HTTP.name} via HTTP and delete all it's devices`, done => {
+        
+        TEST_DEVICE_TYPES.HTTP.id = null;
+        const deviceTypeModel = new DeviceType(TEST_DEVICE_TYPES.HTTP);
+
+        httpDeviceHive.deviceType.insert(deviceTypeModel)
+            .then(({ id }) => {
+                TEST_DEVICE_TYPES.HTTP.id = id;
+                TEST_DEVICES.HTTP.deviceTypeId = id;
+            })
+            .then(() => {
+                const deviceModel = new Device(TEST_DEVICES.HTTP);
+                return httpDeviceHive.device.add(deviceModel);
+            })
+            .then(() => {
+                const deviceTypeDeleteQuery = new DeviceTypeDeleteQuery({ deviceTypeId: TEST_DEVICE_TYPES.HTTP.id, force: true });
+                return httpDeviceHive.deviceType.delete(deviceTypeDeleteQuery)
+            })
+            .then(() => httpDeviceHive.device.get(TEST_DEVICES.HTTP.id))
+            .catch(err => {
+                assert.equal(err,`Device with such deviceId = ${TEST_DEVICES.HTTP.id} not found`);
+                done();
+            });
+
+    });
+
+    it(`should delete device type with name: ${TEST_DEVICE_TYPES.WS.name} via WS and delete all it's devices`, done => {
+
+        TEST_DEVICE_TYPES.WS.id = null;
+
+        const deviceTypeModel = new DeviceType(TEST_DEVICE_TYPES.WS);
+
+        wsDeviceHive.deviceType.insert(deviceTypeModel)
+            .then(({ id }) => {
+                console.log('wow');
+                TEST_DEVICE_TYPES.WS.id = id;
+                TEST_DEVICES.WS.deviceTypeId = id;
+            })
+            .then(() => {
+                const deviceModel = new Device(TEST_DEVICES.WS);
+                return wsDeviceHive.device.add(deviceModel);
+            })
+            .then(() => {
+                const deviceTypeDeleteQuery = new DeviceTypeDeleteQuery({ deviceTypeId: TEST_DEVICE_TYPES.WS.id, force: true });
+                return wsDeviceHive.deviceType.delete(deviceTypeDeleteQuery)
+            })
+            .then(() => wsDeviceHive.device.get(TEST_DEVICES.WS.id))
+            .catch(err => {
+                assert.equal(err,`Device with such deviceId = ${TEST_DEVICES.WS.id} not found`);
+                done();
+            });
     });
 
     after(done => {
